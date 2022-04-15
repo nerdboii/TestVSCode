4316ff6eee09efa94801cb4b452a09fba743fc1e
nerdboii <poseidon21th2@gmail.com>
2022-04-15 19:49:18 +0700

    All bugs fixed

--- Test.sol
+++ Test.sol
@@ -1,1 +1,13 @@
-pragma solidity ^0.6.0;
+pragma solidity 0.8.13;
+
+contract testGit {
+    address _serverAddress;
+    string _message;
+
+    event pingToServer(address sender, address server, string context);
+
+    function Ping(address serverAddress, string memory context) public view {
+        _sender = msg.sender;
+        emit(_sender, serverAddress, context);
+    }
+}
